bf2c5d71ff59509e6602e1b991c3ed154d6ae402
OJIMEP <post_lv@mail.ru>
2022-09-15 18:21:51 +0300

    Add project files.

---
+++ GiftCertControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using GiftCertificateService.Contracts.V1.Responses;
+
+namespace GiftSertificateService.IntegrationTest
+{
+    public class GiftCertControllerTest : IntegrationTest
+    {
+        [Fact]
+        public async Task GetInfoAsync_WithValidBarcode()
+        {
+            // Arrange 
+            await AuthenticateAsync();
+
+            // Act 
+            var response = await TestClient.GetAsync("api/GiftCert?barcode=CC13AVC5Yrw");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var result = await response.Content.ReadAsAsync<CertGetResponse>();
+            result.Barcode.Should().Be("CC13AVC5Yrw");
+        }
+
+        [Theory]
+        [InlineData("api/GiftCert?barcode=CC13AVC5YRK", "Certs aren't valid")]
+        [InlineData("api/GiftCert?barcode=CC13AVC5YRK1", "Cert's barcode should be 11 symbols length")]
+        [InlineData("api/GiftCert?barcode=CC13AVC5Kдг", "Cert's barcode is in wrong format - only latin symbols and digits are allowed")]
+        public async Task GetInfoAsync_WithInvalidBarcode(string query, string expected)
+        {
+            // Arrange 
+            await AuthenticateAsync();
+
+            // Act 
+            var response = await TestClient.GetAsync(query);
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+            (await response.Content.ReadAsAsync<ErrorResponse>()).Error.Should().Be(expected);
+        }
+    }
+}
---
+++ IntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using DateTimeService.Areas.Identity.Models;
+using GiftCertificateService;
+using GiftCertificateService.Contracts.V1.Responses;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Headers;
+
+namespace GiftSertificateService.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Program>();
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync("api/Authenticate/login", new LoginModel
+            {
+                Username = "admin@test.com",
+                Password = "qwert1"
+            });
+
+            var loginResponse = await response.Content.ReadAsAsync<LoginResponse>();
+            return loginResponse.Token;
+        }
+    }
+}
